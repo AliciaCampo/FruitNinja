8ebb6bc1f32c977c1c840ae031000ae109b3dcee
Maria Manzano Roldán <2023_maria.manzano@iticbcn.cat>
2025-04-25 18:12:36 +0200

    Fets tots els assets al AssetManager i el MainGame

--- core/src/main/java/m8/uf3/projecte/MainGame.java
+++ core/src/main/java/m8/uf3/projecte/MainGame.java
@@ -1,34 +1,23 @@
 package m8.uf3.projecte;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
+import m8.uf3.projecte.helpers.AssetManager;
+
+import m8.uf3.projecte.screens.StartScreen;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class MainGame extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+public class MainGame extends Game {
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-    }
+        AssetManager.load();
+        setScreen(new StartScreen(this));
 
-    @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        super.dispose();
+        AssetManager.dispose();
     }
 }
--- core/src/main/java/m8/uf3/projecte/helpers/AssetManager.java
+++ core/src/main/java/m8/uf3/projecte/helpers/AssetManager.java
@@ -1,4 +1,66 @@
 package m8.uf3.projecte.helpers;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+
 public class AssetManager {
+    public  static Texture manzana, pera, limon, naranja, sandia, melon, pina, bomba, moneda, corazon, explosion;
+    public static Texture manzanaC, peraC, limonC, naranjaC, sandiaC, melonC, pinaC;
+    public static Texture fondoStart, fondoJuego;
+    public static Sound explBomba, musicaFondo, corte;
+    public static BitmapFont font;
+    public static void load() {
+        manzana = new Texture(Gdx.files.internal("manzana_entera.png"));
+        pera = new Texture(Gdx.files.internal("pera_entera.png"));
+        limon = new Texture(Gdx.files.internal("limon_entero.png"));
+        naranja = new Texture(Gdx.files.internal("naranja_entera.png"));
+        sandia = new Texture(Gdx.files.internal("sandia_entera.png"));
+        melon = new Texture(Gdx.files.internal("melon_entero.png"));
+        pina = new Texture(Gdx.files.internal("pina_entera.png"));
+        bomba = new Texture(Gdx.files.internal("bomb.png"));
+        moneda = new Texture(Gdx.files.internal("coin.png"));
+        corazon = new Texture(Gdx.files.internal("heart.png"));
+        explosion = new Texture(Gdx.files.internal("explosion.png"));
+        manzanaC = new Texture(Gdx.files.internal("manzana_cortada.png"));
+        peraC = new Texture(Gdx.files.internal("pera_cortada"));
+        limonC = new Texture(Gdx.files.internal("limon_cortado.png"));
+        naranjaC = new Texture(Gdx.files.internal("naranja_cortada.png"));
+        sandiaC = new Texture(Gdx.files.internal("sandia_cortada.png"));
+        melonC = new Texture(Gdx.files.internal("melon_cortado.png"));
+        pinaC = new Texture(Gdx.files.internal("pina_cortada.png"));
+        fondoStart = new Texture(Gdx.files.internal("fondo_inicio.png"));
+        fondoJuego = new Texture(Gdx.files.internal("fondo_juego2.png"));
+        explBomba = Gdx.audio.newSound(Gdx.files.internal("bombExplodeFinal.wav"));
+        musicaFondo = Gdx.audio.newSound(Gdx.files.internal("fruitNinjaMusicBackgroundExtended.mp3"));
+        corte = Gdx.audio.newSound(Gdx.files.internal("swordSwipe2.wav"));
+    }
+
+    public static void dispose() {
+        manzana.dispose();
+        pera.dispose();
+        limon.dispose();
+        naranja.dispose();
+        sandia.dispose();
+        melon.dispose();
+        pina.dispose();
+        bomba.dispose();
+        moneda.dispose();
+        corazon.dispose();
+        explosion.dispose();
+        manzanaC.dispose();
+        peraC.dispose();
+        limonC.dispose();
+        naranjaC.dispose();
+        sandiaC.dispose();
+        melonC.dispose();
+        pinaC.dispose();
+        fondoStart.dispose();
+        fondoJuego.dispose();
+        explBomba.dispose();
+        musicaFondo.dispose();
+        corte.dispose();
+        font.dispose();
+    }
 }
